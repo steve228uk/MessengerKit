f3786e4f74558cfe0708daffe95a3246970c6dbe
Mohammad Khakpaki <mohamad.khakpaki@gmail.com>
2018-11-13 11:54:40 +0330

    Support for RTL placeholder

--- MessengerKit/Input/MSGPlaceholderTextView.swift
+++ MessengerKit/Input/MSGPlaceholderTextView.swift
@@ -125,6 +125,7 @@ open class MSGPlaceholderTextView: UIView {
         label.text = placeholder
         label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
         label.textColor = .lightGray
+        label.decideTextDirection()
     }
     
     private func styleTextView() {
@@ -156,4 +157,19 @@ public protocol MSGPlaceholderTextViewDelegate: NSObjectProtocol {
     
     func textViewDidChange(_ textView: UITextView)
     
+}
+extension UILabel {
+    func decideTextDirection () {
+        let tagScheme = [NSLinguisticTagScheme.language]
+        let tagger    = NSLinguisticTagger(tagSchemes: tagScheme, options: 0)
+        tagger.string = self.text
+        let lang = tagger.tag(at: 0, scheme: NSLinguisticTagScheme.language,
+                              tokenRange: nil, sentenceRange: nil)
+        
+        if lang?.rawValue.range(of:"ar") != nil {
+            self.textAlignment = NSTextAlignment.right
+        } else {
+            self.textAlignment = NSTextAlignment.left
+        }
+    }
 }
