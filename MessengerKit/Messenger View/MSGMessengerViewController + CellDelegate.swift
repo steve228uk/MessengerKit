b274009fd38a1cf4111bab2a38a99f34d7577d1b
Mohammad Khakpaki <mohamad.khakpaki@gmail.com>
2018-11-21 14:35:52 +0330

    removed opening url by default when clicked

--- MessengerKit/Messenger View/MSGMessengerViewController + CellDelegate.swift
+++ MessengerKit/Messenger View/MSGMessengerViewController + CellDelegate.swift
@@ -19,24 +19,7 @@ extension MSGMessengerViewController: MSGMessageCellDelegate {
     }
     
     public func cellLinkTapped(url: URL) {
-        
-        guard let delegate = delegate else { return }
-        
-        if let scheme = url.scheme, scheme.starts(with: "http"),
-        delegate.shouldDisplaySafari(for: url) {
-            
-            let vc = SFSafariViewController(url: url)
-            vc.preferredControlTintColor = tintColor
-            present(vc, animated: true)
-            
-        } else if delegate.shouldOpen(url: url) {
-            
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-        
-        }
-
-        delegate.linkTapped(url: url)
-        
+        delegate?.linkTapped(url: url)
     }
     
     public func cellAvatarTapped(for user: MSGUser) {
